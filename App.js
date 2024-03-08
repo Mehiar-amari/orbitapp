4d43fe6c0479a701b092f257830f445415568e52
Yassine Khlify <yessine.khelifi@istic.ucar.tn>
2024-03-08 16:03:07 +0100

    changes

--- App.js
+++ App.js
@@ -3,21 +3,39 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import SignInScreen from './screens/SignInScreen/SignInScreen';
 //import CustomInput from './components/custominput/CustomInput';
 import SignUpScreen from './screens/SignUpScreen/SignUpScreen';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavPage from './screens/NavPage/NavPage';
 import PageH from './screens/HomePages/PageH';
 
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+function DrawerContent({ navigation }) {
+  const navigateToScreen = (screenName) => {
+    navigation.navigate(screenName);
+    navigation.closeDrawer();
+  };
 
-const Stack = createNativeStackNavigator()
+  return (
+    <SafeAreaView style={styles.drawerContainer}>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('NavPage')}>
+        <Text>NavPage</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
+        <Text>PageH</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaView style={styles.root}>
-        <Stack.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Stack.Navigator screenOptions={{headerTitle: ''}}>
           <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
           <Stack.Screen name="NavPage" component={NavPage} />
           <Stack.Screen name="PageH" component={PageH} />
--- screens/HomePages/PageH.js
+++ screens/HomePages/PageH.js
@@ -7,6 +7,11 @@ import { FontAwesome, Octicons } from '@expo/vector-icons';
 const PageH = () => {
   const [selectedMonth1, setSelectedMonth1] = useState('');
   const [selectedMonth2, setSelectedMonth2] = useState('');
+  const [selectedMonth3, setSelectedMonth3] = useState('');
+  const [selectedMonth4, setSelectedMonth4] = useState('');
+  const [selectedMonth5, setSelectedMonth5] = useState('');
+  const [selectedMonth6, setSelectedMonth6] = useState('');
+  
   const months = [
     'January', 'February', 'March', 'April',
     'May', 'June', 'July', 'August',
@@ -19,12 +24,30 @@ const PageH = () => {
     setSelectedMonth1(month);
     setOpenPicker(null);
   };
+  
 
   const handleMonthClick2 = (month) => {
     setSelectedMonth2(month);
     setOpenPicker(null);
   };
+  const handleMonthClick3 = (month) => {
+    setSelectedMonth3(month);
+    setOpenPicker(null);
+  };
 
+  const handleMonthClick4 = (month) => {
+    setSelectedMonth4(month);
+    setOpenPicker(null);
+  };
+  const handleMonthClick5 = (month) => {
+    setSelectedMonth5(month);
+    setOpenPicker(null);
+  };
+  const handleMonthClick6 = (month) => {
+    setSelectedMonth6(month);
+    setOpenPicker(null);
+  };
+  
   const renderMonthOptions = () => {
     return months.map((month, index) => (
       <TouchableOpacity key={index} onPress={() => handleMonthClick1(month)}>
@@ -40,20 +63,50 @@ const PageH = () => {
       </TouchableOpacity>
     ));
   };
+  const renderMonthOptions3 = () => {
+    return months.map((month, index) => (
+      <TouchableOpacity key={index} onPress={() => handleMonthClick3(month)}>
+        <Text style={styles.monthOption}>{month}</Text>
+      </TouchableOpacity>
+    ));
+  };
+  const renderMonthOptions4 = () => {
+    return months.map((month, index) => (
+      <TouchableOpacity key={index} onPress={() => handleMonthClick4(month)}>
+        <Text style={styles.monthOption}>{month}</Text>
+      </TouchableOpacity>
+    ));
+  };
+  const renderMonthOptions5 = () => {
+    return months.map((month, index) => (
+      <TouchableOpacity key={index} onPress={() => handleMonthClick5(month)}>
+        <Text style={styles.monthOption}>{month}</Text>
+      </TouchableOpacity>
+    ));
+  };
+  const renderMonthOptions6 = () => {
+    return months.map((month, index) => (
+      <TouchableOpacity key={index} onPress={() => handleMonthClick6(month)}>
+        <Text style={styles.monthOption}>{month}</Text>
+      </TouchableOpacity>
+    ));
+  };
+
+  
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: COLORS.white }}>
       <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', marginTop: 20 }}>
-        <View style={{ width: '90%', height: 300, backgroundColor: COLORS.white, borderRadius: 1, marginTop: 1, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 }}>
-          <View style={{ flex: 2 / 5, justifyContent: 'center', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: COLORS.lightGray2, overflow: 'hidden', width: '100%', height: '100%' }}>
+        <View style={{ width: '90%', height: 300, backgroundColor: COLORS.white, borderRadius: 10, marginTop: 1, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 ,zIndex:2}}>
+          <View style={{ flex: 1 / 2, justifyContent: 'center', alignItems: 'center',borderRadius: 10, borderBottomWidth: 1, borderBottomColor: COLORS.lightGray2, overflow: 'hidden', width: '100%', height: '100%' }}>
             <Image
-              source={require('../../assets/image1.png')}
-              style={{ width: '100%', height: 'auto', aspectRatio: 12 / 8, resizeMode: 'contain' }}
+              source={require('../../assets/warda2.jpg')}
+              style={{ width: '100%', height: 'auto', aspectRatio: 12 / 8, resizeMode: 'cover' }}
             />
             <View style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 10 }}>
               <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
                 <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
-                  <Text style={{ color: 'white', textAlign: 'left' }}>Your Text Here</Text>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>Pates Warda Trigeneration</Text>
                 </View>
                 <View style={{ flexDirection: 'column', alignItems: 'flex-end', marginLeft: 'auto' }}>
                   <Ionicons name="desktop-outline" size={24} color="white" />
@@ -61,13 +114,13 @@ const PageH = () => {
               </View>
             </View>
           </View>
-          <View style={{ flex: 3 / 5, paddingHorizontal: 20 }}>
+          <View style={{ flex: 1 / 2, paddingHorizontal: 20 }}>
             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
               <View>
                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                   <MaterialIcons name="bolt" size={15} color="orange" style={{ marginRight: 8 }} />
                   <View>
-                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>energy</Text>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Energy</Text>
                   </View>
                 </View>
               </View>
@@ -79,11 +132,15 @@ const PageH = () => {
                       <Text>▼</Text>
                     </View>
                   </TouchableOpacity>
+                  <View style={{zIndex: 1,position:'relative'}}>
                   {openPicker === 1 && (
                     <View style={styles.monthOptionsContainer}>
                       {renderMonthOptions()}
                     </View>
                   )}
+
+                  </View>
+                  
                 </View>
               </View>
               <View style={{ marginLeft: 'auto' }}>
@@ -95,9 +152,9 @@ const PageH = () => {
             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
               <View>
                 <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                  <MaterialIcons name="bolt" size={15} color="orange" style={{ marginRight: 8 }} />
+                  <Ionicons name="flame" size={15} color="orange" style={{ marginRight: 8 }} />
                   <View>
-                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>gas</Text>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Gas</Text>
                   </View>
                 </View>
               </View>
@@ -124,12 +181,181 @@ const PageH = () => {
             </View>
           </View>
         </View>
+        <View style={{height: 20}}/>
+        <View style={{ width: '90%', height: 300, backgroundColor: COLORS.white, borderRadius: 10, marginTop: 1, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 ,zIndex:1}}>
+          <View style={{ flex: 1 / 2, justifyContent: 'center', alignItems: 'center',borderRadius: 10, borderBottomWidth: 1, borderBottomColor: COLORS.lightGray2, overflow: 'hidden', width: '100%', height: '100%' }}>
+            <Image
+              source={require('../../assets/warda2.jpg')}
+              style={{ width: '100%', height: 'auto', aspectRatio: 12 / 8, resizeMode: 'cover' }}
+            />
+            <View style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 10 }}>
+              <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+                <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>Pates Warda Trigeneration</Text>
+                </View>
+                <View style={{ flexDirection: 'column', alignItems: 'flex-end', marginLeft: 'auto' }}>
+                  <Ionicons name="desktop-outline" size={24} color="white" />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={{ flex: 1 / 2, paddingHorizontal: 20 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+              <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <MaterialIcons name="bolt" size={15} color="orange" style={{ marginRight: 8 }} />
+                  <View>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Energy</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={{ marginLeft: 30 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => setOpenPicker(3)}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Text style={{ ...FONTS.body4, fontSize: 13, marginRight: 5 }}>{selectedMonth3 || 'Select:Month'}</Text>
+                      <Text>▼</Text>
+                    </View>
+                  </TouchableOpacity>
+                  {openPicker === 3 && (
+                    <View style={styles.monthOptionsContainer}>
+                      {renderMonthOptions3()}
+                    </View>
+                  )}
+                </View>
+              </View>
+              <View style={{ marginLeft: 'auto' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ ...FONTS.body4, fontSize: 13 }}>159.327 mwh</Text>
+                </View>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+              <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Ionicons name="flame" size={15} color="orange" style={{ marginRight: 8 }} />
+                  <View>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Gas</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={{ marginLeft: 30 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => setOpenPicker(4)}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Text style={{ ...FONTS.body4, fontSize: 13, marginRight: 5 }}> {selectedMonth4 || 'Select:Month'}</Text>
+                      <Text>▼</Text>
+                    </View>
+                  </TouchableOpacity>
+                  {openPicker === 4 && (
+                    <View style={styles.monthOptionsContainer}>
+                      {renderMonthOptions4()}
+                    </View>
+                  )}
+                </View>
+              </View>
+              <View style={{ marginLeft: 'auto' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ ...FONTS.body4, fontSize: 13 }}>159.327 mwh</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={{height: 20}}/>
+        <View style={{ width: '90%', height: 300, backgroundColor: COLORS.white, borderRadius: 10, marginTop: 1, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 ,zIndex:0}}>
+          <View style={{ flex: 1 / 2, justifyContent: 'center', alignItems: 'center',borderRadius: 10, borderBottomWidth: 1, borderBottomColor: COLORS.lightGray2, overflow: 'hidden', width: '100%', height: '100%' }}>
+            <Image
+              source={require('../../assets/warda2.jpg')}
+              style={{ width: '100%', height: 'auto', aspectRatio: 12 / 8, resizeMode: 'cover' }}
+            />
+            <View style={{ position: 'absolute', bottom: 0, width: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 10 }}>
+              <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+                <View style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>Pates Warda Trigeneration</Text>
+                </View>
+                <View style={{ flexDirection: 'column', alignItems: 'flex-end', marginLeft: 'auto' }}>
+                  <Ionicons name="desktop-outline" size={24} color="white" />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={{ flex: 1 / 2, paddingHorizontal: 20 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+              <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <MaterialIcons name="bolt" size={15} color="orange" style={{ marginRight: 8 }} />
+                  <View>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Energy</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={{ marginLeft: 30 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => setOpenPicker(5)}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Text style={{ ...FONTS.body4, fontSize: 13, marginRight: 5 }}>{selectedMonth5 || 'Select:Month'}</Text>
+                      <Text>▼</Text>
+                    </View>
+                  </TouchableOpacity>
+                  {openPicker === 5 && (
+                    <View style={styles.monthOptionsContainer}>
+                      {renderMonthOptions5()}
+                    </View>
+                  )}
+                </View>
+              </View>
+              <View style={{ marginLeft: 'auto' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ ...FONTS.body4, fontSize: 13 }}>159.327 mwh</Text>
+                </View>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+              <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Ionicons name="flame" size={15} color="orange" style={{ marginRight: 8 }} />
+                  <View>
+                    <Text style={{ ...FONTS.body4, fontSize: 13 }}>Gas</Text>
+                  </View>
+                </View>
+              </View>
+              <View style={{ marginLeft: 30 }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => setOpenPicker(6)}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Text style={{ ...FONTS.body4, fontSize: 13, marginRight: 5 }}> {selectedMonth6 || 'Select:Month'}</Text>
+                      <Text>▼</Text>
+                    </View>
+                  </TouchableOpacity>
+                  {openPicker === 6 && (
+                    <View style={styles.monthOptionsContainer}>
+                      {renderMonthOptions6()}
+                    </View>
+                  )}
+                </View>
+              </View>
+              <View style={{ marginLeft: 'auto' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Text style={{ ...FONTS.body4, fontSize: 13 }}>159.327 mwh</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View style={{height: 400}}/>
+
+        
+        
       </View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+ 
   monthOptionsContainer: {
     position: 'absolute',
     top: '100%', // Place it below the parent element
@@ -142,6 +368,7 @@ const styles = StyleSheet.create({
     zIndex: 3, // Ensure it's on top
   },
   monthOption: {
+    
     paddingVertical: 5,
     paddingHorizontal: 10,
     fontSize: 16,
