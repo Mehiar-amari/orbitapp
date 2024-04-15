bc9e84137db38957a12ad6228c7eca4090b25e74
Yassine Khlify <yessine.khelifi@istic.ucar.tn>
2024-04-15 19:05:02 +0100

    li zedhom housem

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, TouchableOpacity, View ,Image} from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, View, Image } from 'react-native';
 import SignInScreen from './screens/SignInScreen/SignInScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,12 +7,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavPage from './screens/NavPage/NavPage';
 import PageH from './screens/HomePages/PageH';
 import { Ionicons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons'; // Import MaterialIcons from Expo Vector Icons
 import { Octicons } from '@expo/vector-icons'; // Import MaterialIcons from Expo Vector Icons
 import { FontAwesome5 } from '@expo/vector-icons'; // Import MaterialIcons from Expo Vector Icons
 import NotificationScreen from './screens/NotificationScreen'; // Import the new screen component
 import registerNNPushToken from 'native-notify';
+
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -25,38 +26,38 @@ function DrawerContent({ navigation }) {
 
   return (
     <SafeAreaView style={styles.drawerContainer}>
-  {/* Empty space */}
-  <Image source={require('./assets/orbit.png')} style={styles.image} />
-  <View style={styles.emptySpace} />
-  <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('NavPage')}>
-  <MaterialIcons name="settings" size={20} color="white" style={styles.icon} />
-    <Text style={styles.text}>Configuration</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
-  <Ionicons name="mail" size={20} color="white" style={styles.icon} />
-    <Text style={styles.text}>Mailing</Text>
-  </TouchableOpacity>
-  <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
-  <Ionicons name="stats-chart" size={20} color="white" style={styles.icon} />
-    <Text style={styles.text}>Analytics</Text>
-  </TouchableOpacity>
+      {/* Empty space */}
+      <Image source={require('./assets/orbit.png')} style={styles.image} />
+      <View style={styles.emptySpace} />
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('NavPage')}>
+        <MaterialIcons name="settings" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Configuration</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
+        <Ionicons name="mail" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Mailing</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
+        <Ionicons name="stats-chart" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Analytics</Text>
+      </TouchableOpacity>
 
-  <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
-  <FontAwesome5  name="file-invoice-dollar" size={20} color="white" style={styles.icon} />
-    <Text style={styles.text}>Bill simulation</Text>
-  </TouchableOpacity>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
+        <FontAwesome5 name="file-invoice-dollar" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Bill simulation</Text>
+      </TouchableOpacity>
 
-  <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
-  <Ionicons name="document-text-sharp" size={20} color="white" style={styles.icon} />
-    <Text style={styles.text}>Reports</Text>
-  </TouchableOpacity>
-  <View style={styles.bottomContainer}>
-    <TouchableOpacity style={styles.signOutButton} onPress={() => navigateToScreen('SignIn')}>
-      <Ionicons name="log-out-outline" size={20} color="white" style={styles.icon} />
-      <Text style={styles.text}>Sign Out</Text>
-    </TouchableOpacity>
-  </View>
-</SafeAreaView>
+      <TouchableOpacity style={styles.drawerItem} onPress={() => navigateToScreen('PageH')}>
+        <Ionicons name="document-text-sharp" size={20} color="white" style={styles.icon} />
+        <Text style={styles.text}>Reports</Text>
+      </TouchableOpacity>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.signOutButton} onPress={() => navigateToScreen('SignIn')}>
+          <Ionicons name="log-out-outline" size={20} color="white" style={styles.icon} />
+          <Text style={styles.text}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -65,10 +66,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />} >
-        <Drawer.Screen 
-          name="MainStack" 
-          component={MainStackNavigator} 
-          options={{ headerShown: false }} 
+        <Drawer.Screen
+          name="MainStack"
+          component={MainStackNavigator}
+          options={{ headerShown: false }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
@@ -77,15 +78,15 @@ export default function App() {
 
 const MainStackNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{headerTitle: '',}}>
-      <Stack.Screen 
-        name="SignIn" 
-        component={SignInScreen} 
-        options={{ headerShown: false }} 
+    <Stack.Navigator screenOptions={{ headerTitle: '', }}>
+      <Stack.Screen
+        name="SignIn"
+        component={SignInScreen}
+        options={{ headerShown: false }}
       />
-      <Stack.Screen 
-        name="NavPage" 
-        component={NavPage} 
+      <Stack.Screen
+        name="NavPage"
+        component={NavPage}
         options={({ navigation }) => ({
           headerRight: () => (
             <View style={{ flexDirection: 'row', marginRight: 15 }}>
@@ -93,7 +94,7 @@ const MainStackNavigator = () => {
                 onPress={() => navigation.navigate('Notification')} // Navigate to the notification page
                 style={{ marginRight: 15 }}
               >
-                <Ionicons name="notifications" size={24} color="black"/>
+                <Ionicons name="notifications" size={24} color="black" />
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => navigation.openDrawer()}
@@ -105,9 +106,9 @@ const MainStackNavigator = () => {
           ),
         })}
       />
-      <Stack.Screen 
-        name="PageH" 
-        component={PageH} 
+      <Stack.Screen
+        name="PageH"
+        component={PageH}
         options={({ navigation }) => ({
           headerRight: () => (
             <View style={{ flexDirection: 'row', marginRight: 15 }}>
@@ -115,7 +116,7 @@ const MainStackNavigator = () => {
                 onPress={() => navigation.navigate('Notification')} // Navigate to the notification page
                 style={{ marginRight: 15 }}
               >
-                <Ionicons name="notifications" size={24} color="black"/>
+                <Ionicons name="notifications" size={24} color="black" />
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => navigation.openDrawer()}
@@ -127,10 +128,10 @@ const MainStackNavigator = () => {
           ),
         })}
       />
-       <Stack.Screen 
-        name="Notification" 
+      <Stack.Screen
+        name="Notification"
         component={NotificationScreen} // Add the NotificationScreen component here
-        options={{ headerTitle: 'Notifications' ,headerTitleAlign: 'center'}} // Set header title
+        options={{ headerTitle: 'Notifications', headerTitleAlign: 'center' }} // Set header title
       />
       {/* Add other screens here */}
     </Stack.Navigator>
@@ -141,18 +142,18 @@ const styles = StyleSheet.create({
   drawerContainer: {
     flex: 1,
     borderRadius: 40, // Adjust the border radius as needed
- 
-  
+
+
   },
   drawerItem: {
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-    backgroundColor:"#3B51F4",
+    backgroundColor: "#3B51F4",
     borderRadius: 40,
     marginHorizontal: 10, // margin horizontal for better spacing
     marginTop: 10, // margin bottom for better spacing between items
-    shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 ,
+    shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5,
     flexDirection: 'row', // align icon and text horizontally
     alignItems: 'center', // center vertically
   },
@@ -160,13 +161,13 @@ const styles = StyleSheet.create({
     padding: 10,
     borderTopWidth: 1,
     borderTopColor: '#ccc',
-    backgroundColor:"#3B51F4",
+    backgroundColor: "#3B51F4",
     borderRadius: 40,
     marginHorizontal: 10, // margin horizontal for better spacing
     marginBottom: 10, // margin bottom for better spacing between items
     flexDirection: 'row', // align icon and text horizontally
     alignItems: 'center', // center vertically
-    shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 ,
+    shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5,
   },
   bottomContainer: {
     flex: 1,
@@ -175,7 +176,7 @@ const styles = StyleSheet.create({
   emptySpace: {
     height: 0, // Adjust the height as needed
   },
-   icon: {
+  icon: {
     marginRight: 10, // add some space between icon and text
   },
   buttonText: {
