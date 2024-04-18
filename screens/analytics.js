2165565dc32fcf0e8faa0782d611d7972d9813f3
unknown <mehyarlamari@gmail.com>
2024-04-18 19:16:44 +0100

    the button needs to be fixed

--- screens/analytics.js
+++ screens/analytics.js
@@ -1,15 +1,20 @@
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 import React, { useState } from 'react';
-import { Text, View, ScrollView, Dimensions, TouchableWithoutFeedback,TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, ScrollView, Dimensions, TouchableWithoutFeedback,TouchableOpacity, TextInput,Platform,Button ,StyleSheet} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { Badge, Modal } from 'react-native-paper';
-import { DateTimePicker } from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker'
 import { Calendar } from 'react-native-calendars';
+import { Ionicons } from 'react-native-vector-icons'; // Import Ionicons from React Native Vector Icons
+import { FontAwesome } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons'; // Import MaterialIcons from Expo Vector Icons
+import { Octicons } from '@expo/vector-icons'; // Import MaterialIcons from Expo Vector Icons
 
 const Analytics = () => {
   const [selectedItems, setSelectedItems] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [currentValue, setCurrentValue] = useState([]);
+  
 
   const [isOpen2, setIsOpen2] = useState(false);
   const [currentValue2, setCurrentValue2] = useState([]);
@@ -47,11 +52,7 @@ const Analytics = () => {
 
 
 
-  const [selectedDate, setSelectedDate] = useState(null); // State to store the selected date
-  const handleDateSelect = (date) => {
-    setSelectedDate(date);
-    setShowModal(false);
-  };
+  
   
   
   const handlePressOutside = () => {
@@ -79,12 +80,64 @@ const Analytics = () => {
       setIsOpen7(false);
     }
   };
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show , setShow] = useState(false);
+  const [text , setText] = useState('select date');
 
- 
+
+  const onChange = (event, selectedDate) => {
+    
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+
+    let tempDate = new Date(currentDate);
+    let fDate = tempDate.getDate() + '/'+ (tempDate.getMonth()+1)+ '/'+ tempDate.getFullYear();
+    setText(fDate)
+   
+    console.log();
+  };
+const showMode = (currentMode) =>{
+  setShow(true);
+  setMode(currentMode);
+}
+
+
+
+
+
+const [date2, setDate2] = useState(new Date());
+  const [mode2, setMode2] = useState('date');
+  const [show2 , setShow2] = useState(false);
+  const [text2 , setText2] = useState('select date');
+
+
+  const onChange2 = (event, selectedDate2) => {
+    
+    const currentDate2 = selectedDate2 || date;
+    setShow2(false);
+    setDate2(currentDate2);
+
+    let tempDate2 = new Date(currentDate2);
+    let fDate2 = tempDate2.getDate() + '/'+ (tempDate2.getMonth()+1)+ '/'+ tempDate2.getFullYear();
+    setText2(fDate2)
+   
+    console.log();
+  };
+const showMode2 = (currentMode2) =>{
+  setShow2(true);
+  setMode2(currentMode2);
+}
+
+
+
+
+
 
   return (
     <TouchableWithoutFeedback onPress={handlePressOutside}>
-      <ScrollView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
+      <ScrollView style={{ flex: 1, backgroundColor: "#FFFFFF" }} >
         <View style={{ marginTop: 20, paddingLeft: 20 }}>
           <Text style={{ fontSize: 24, fontWeight: "bold", color: "#888888" }}>Configuration</Text>
         </View>
@@ -96,12 +149,13 @@ const Analytics = () => {
             setOpen={() => setIsOpen(!isOpen)}
             value={currentValue}
             setValue={(val) => setCurrentValue(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+            
+            autoScroll
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -119,12 +173,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen2(!isOpen2)}
             value={currentValue2}
             setValue={(val) => setCurrentValue2(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+            
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -145,12 +199,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen3(!isOpen3)}
             value={currentValue3}
             setValue={(val) => setCurrentValue3(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+           
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -172,12 +226,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen4(!isOpen4)}
             value={currentValue4}
             setValue={(val) => setCurrentValue4(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+            
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -198,12 +252,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen5(!isOpen5)}
             value={currentValue5}
             setValue={(val) => setCurrentValue5(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+          
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -222,12 +276,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen6(!isOpen6)}
             value={currentValue6}
             setValue={(val) => setCurrentValue6(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+            
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -244,12 +298,12 @@ const Analytics = () => {
             setOpen={() => setIsOpen7(!isOpen7)}
             value={currentValue7}
             setValue={(val) => setCurrentValue7(val)}
-            maxHeight={200}
+            maxHeight={300}
             placeholder='select an item'
             showTickIcon={true}
             dropDownDirection='bottom'
             multiple={true}
-            min={1}
+           
             mode="BADGE"
             badgeColors={"#A446CB"}
             badgeDotColors={"white"}
@@ -259,33 +313,80 @@ const Analytics = () => {
         </View>
 
       
-  <View style={{ marginTop: 30, paddingLeft: 20 }}>
-      <Text style={{ fontSize: 14, color: '#333', marginBottom: 10 }}>Select Date Debut</Text>
-      <TouchableOpacity onPress={() => setShowModal(true)}>
-        <View style={{ borderWidth: 1, borderRadius: 10, padding: 10 }}>
-          <Text>{selectedDate ? selectedDate.toDateString() : "Select date"}</Text>
-        </View>
-      </TouchableOpacity>
-      <Modal visible={showModal} animationType="fade" transparent={true} statusBarTranslucent={true}>
-        <TouchableWithoutFeedback onPress={() => setShowModal(false)}>
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' , marginTop: 120}}>
-            <View style={{ backgroundColor: 'white', borderRadius: 10, padding: 20, width: screenWidth - 40, marginTop: 150 }}>
-              <TouchableOpacity onPress={() => setShowModal(false)} style={{ position: 'absolute', top: 10, right: 10, zIndex: 100001 }}>
-                <Text style={{ fontSize: 18, color: 'black' }}>Close</Text>
-              </TouchableOpacity>
-              <Calendar onDateSelect={handleDateSelect} />
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
+        <View style={{ marginTop: 30, paddingLeft: 20 }}>
+  <Text style={{ fontSize: 14, color: '#333', marginBottom: 10 }}>Select Date Debut</Text>
+  <TouchableOpacity onPress={() => showMode('date')}>
+    <View style={{ borderWidth: 1, borderRadius: 10, padding: 10 }}>
+      <Text>{text}</Text>
     </View>
+  </TouchableOpacity>
+  {show && (
+    <DateTimePicker
+      value={date}
+      mode={mode}
+      is24Hour={true}
+      display="default"
+      onChange={onChange}
+    />
+  )}
+</View>
+
+
+
+
+
+<View style={{ marginTop: 30, paddingLeft: 20 }}>
+  <Text style={{ fontSize: 14, color: '#333', marginBottom: 10 }}>Select Date fin</Text>
+  <TouchableOpacity onPress={() => showMode2('date')}>
+    <View style={{ borderWidth: 1, borderRadius: 10, padding: 10 }}>
+      <Text>{text2}</Text>
+    </View>
+  </TouchableOpacity>
+  {show2 && (
+    <DateTimePicker
+      value={date2}
+      mode={mode2}
+      is24Hour={true}
+      display="default"
+      onChange={onChange2}
+    />
+  )}
+</View>
+
+
+
+<TouchableOpacity style={styles.button}>
+      <Ionicons name="send" size={24} color="white" />
+      <Text style={styles.text}>Send</Text>
+    </TouchableOpacity>
+
+
 
        <View  style={{ marginTop: 500, paddingLeft: 20 }}></View>
 
-     
+
+
+
       </ScrollView>
     </TouchableWithoutFeedback>
   );
 }
 
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    backgroundColor: '#007bff', // Blue background color
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 5, // Adjusted paddingHorizontal value
+    borderRadius: 5,
+  },
+  text: {
+    color: 'white', // White text color
+    marginLeft: 5,
+    fontWeight: 'bold',
+  },
+});
 export default Analytics;
